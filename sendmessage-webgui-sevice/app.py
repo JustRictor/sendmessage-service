56ba0e9f13644dd85acafd5ee75616500efbe534
Kuznetsova Lera <kuzlera@mail.ru>
2024-05-18 00:25:23 +0300

    добавление инпута токена и черновой вариант кнопки получения токена

--- sendmessage-webgui-sevice/app.py
+++ sendmessage-webgui-sevice/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -6,13 +7,24 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        phone = request.form['phone']
-        code = request.form['code']
+        phone = request.form['phone']  # Чтение из инпута телефона в переменную
+        code = request.form['code']  # Чтение из инпута кода в переменную
+        api = request.form['apikey']  # Чтение из инпута апи ключа в переменную
         # Здесь можно обработать данные из формы
-        takedata = [phone, code]
-        print(takedata)
-        return render_template('form.html')
+        take_data = [phone, code, api]  # Запись все, что прочитали в список
+        print(take_data)
+        return render_template('form.html')  # Очистка формы
     return render_template('form.html')
+
+
+@app.route('/get-token', methods=['POST'])
+def get_token():
+    data = request.get_json()
+    api = data.get('api')
+    # response = requests.get('http://docker-sendmessage-service:5000/get-api')
+    # api_from_service = response.json().get('api')
+    token = "sample_token"  # Заглушка для токена
+    return jsonify({'token': token})
 
 
 if __name__ == '__main__':
