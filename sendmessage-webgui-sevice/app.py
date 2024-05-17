bca45d3633a4950eb94322b262d350d00783c20c
Kuznetsova Lera <kuzlera@mail.ru>
2024-05-17 22:44:06 +0300

    обработчик кнопки и считывание инпутов

--- sendmessage-webgui-sevice/app.py
+++ sendmessage-webgui-sevice/app.py
@@ -1,13 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        phone = request.form['phone']
+        code = request.form['code']
+        # Здесь можно обработать данные из формы
+        takedata = [phone, code]
+        print(takedata)
+        return render_template('form.html')
     return render_template('form.html')
 
 
-app.run(
-    debug=True)  # debug true задаем специально для разработки (в данном случае при обновление/изменение кода приложение автоматически само обновит данные на сайте)
+if __name__ == '__main__':
+    app.run(debug=True)
