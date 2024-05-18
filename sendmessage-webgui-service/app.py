1cda336e5cce61f7a1a697f782ab55506a650743
Lera Kuznetsova <kuzlera@mail.ru>
2024-05-18 16:38:50 +0300

    Исправленная авторизация

--- sendmessage-webgui-service/app.py
+++ sendmessage-webgui-service/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, session
 import requests
+from functools import wraps
 
 app = Flask(__name__)
-
 app.secret_key = 'your_secret_key'
 ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = 'password'
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            flash('Вам нужно войти, чтобы получить доступ к этой странице.')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,23 +24,23 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            session['logged_in'] = True
             return redirect(url_for('admin_panel'))
         else:
-            flash('Invalid credentials. Please try again.')
+            flash('Неправильные учетные данные. Попробуйте снова.')
     return render_template('login.html')
 
 
-
 @app.route('/admin-panel', methods=['GET', 'POST'])
+@login_required
 def admin_panel():
     if request.method == 'POST':
-        phone = request.form['phone']  # Чтение из инпута телефона в переменную
-        code = request.form['code']  # Чтение из инпута кода в переменную
-        api = request.form['apikey']  # Чтение из инпута апи ключа в переменную
-        # Здесь можно обработать данные из формы
-        take_data = [phone, code, api]  # Запись все, что прочитали в список
+        phone = request.form['phone']
+        code = request.form['code']
+        api = request.form['apikey']
+        take_data = [phone, code, api]
         print(take_data)
-        return render_template('form.html')  # Очистка формы
+        return render_template('form.html')
     return render_template('form.html')
 
 
@@ -38,9 +48,7 @@ def admin_panel():
 def get_token():
     data = request.get_json()
     api = data.get('api')
-    # response = requests.get('http://docker-sendmessage-service:5000/get-api')
-    # api_from_service = response.json().get('api')
-    token = "sample_token"  # Заглушка для токена
+    token = "sample_token"
     return jsonify({'token': token})
 
 
