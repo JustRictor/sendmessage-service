24c87c43349bc2c9cfb0480ed3d37ff97014c3fa
SleepyOwl703 <anastasiaiva574@gmail.com>
2024-05-18 16:26:25 +0300

    проверки

--- sendmessage-webgui-sevice/HTTPClient.py
+++ sendmessage-webgui-sevice/HTTPClient.py
@@ -1,12 +1,10 @@
-from flask import Flask
 import requests
 import datetime
 
 class HTTPClient:
-    app = Flask(__name__)
     log = []
 
-    URL = 'docker-sendmessage-service:5000/'
+    URL = 'http://192.168.31.17:5000/'
     def __init__(self, phone, msg, token):
         self.phone = phone
         self.msg = msg
@@ -15,14 +13,14 @@ class HTTPClient:
     def get_token(self):
         current_time = datetime.datetime.now()
 
-        self.token = str(requests.get(f'{self.URL}token').text)
+        self.token = str(requests.get(f'{self.URL}').text)
         self.log.append([self.token, current_time])
         print(self.token)
 
 
     def send_data(self):
         self.get_token()
-        r = requests.post(f'{self.URL}get-data',data={'token': self.token, 'phone':self.phone,'msg':self.msg})
+        r = requests.post(f'{self.URL}get-api',data={'token': self.token, 'phone':self.phone,'msg':self.msg})
         print(r.text)
 
     def get_log(self):
@@ -32,5 +30,5 @@ class HTTPClient:
 
 client = HTTPClient("890394935", "1234","Rfudrb#jfj")
 client.get_token()
-client.send_data()
+#client.send_data()
 
