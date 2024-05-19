0e16d1cc49a7ba3975f6a8d5defccb90290e0223
JustRictor <justrictor39@outlook.com>
2024-05-19 15:20:30 +0300

    простой логгер

--- sendmessage-service/src/sendmessage-service/logger/console_logger.cpp
+++ sendmessage-service/src/sendmessage-service/logger/console_logger.cpp
@@ -1,5 +1,22 @@
 #include "console_logger.hpp"
+#include <QDateTime>
+#include <QDebug>
 
 ConsoleLogger::ConsoleLogger(QObject *parent)
     : QObject{parent}
-{}
+    , logFile("sendmessage-service.log")
+{
+    if(!logFile.open(QFile::OpenModeFlag::Append))
+        qDebug() << QDateTime::currentDateTime().toString("dd:MM:yy hh:mm:ss -")
+                 << "cant create/open log file";
+}
+
+void ConsoleLogger::log(const QString &message)
+{
+    QString logmess = QString("%1 - %2")
+                          .arg( QDateTime::currentDateTime().toString("dd:MM:yy hh:mm:ss") )
+                          .arg( message )
+        ;
+    qDebug() << logmess;
+    logFile.write(logmess.toUtf8());
+}
--- sendmessage-service/src/sendmessage-service/logger/console_logger.hpp
+++ sendmessage-service/src/sendmessage-service/logger/console_logger.hpp
@@ -2,12 +2,29 @@
 #define __CONSOLE_LOGGER_HPP_
 
 #include <QObject>
+#include <QString>
+#include <QFile>
 
 class ConsoleLogger : public QObject
 {
     Q_OBJECT
+private:
+    QFile logFile;
 public:
-    explicit ConsoleLogger(QObject *parent = nullptr);
+    static ConsoleLogger& getInstance() {
+        static ConsoleLogger instance{};
+        return instance;
+    }
+    void log(QString const& message);
+
+private:
+    explicit ConsoleLogger(QObject *parent = nullptr)
+        : QObject{parent}
+    {}
+    explicit ConsoleLogger(ConsoleLogger& other) = delete;
+    explicit ConsoleLogger(ConsoleLogger&& other) = delete;
+    ConsoleLogger operator=(ConsoleLogger& other) = delete;
+    ConsoleLogger operator=(ConsoleLogger&& other) = delete;
 
 signals:
 };
